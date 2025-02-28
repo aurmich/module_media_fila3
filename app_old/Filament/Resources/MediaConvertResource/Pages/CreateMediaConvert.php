4466a6c255be7863363056e2c897c18c4ee22867
marco76tv <marco.sottana@gmail.com>
2025-02-28 10:27:57 +0100

    Here is a possible commit message:
    "Added support for media conversion and temporary uploads in Filament resources. This includes the creation of new pages, forms, and actions to manage media conversions and temporary uploads. The code also includes some minor improvements and bug fixes."
    
    This commit message provides a brief summary of the changes made in this commit, including the addition of new features and improvements.

--- app_old/Filament/Resources/MediaConvertResource/Pages/CreateMediaConvert.php
+++ app_old/Filament/Resources/MediaConvertResource/Pages/CreateMediaConvert.php
@@ -6,9 +6,14 @@ namespace Modules\Media\Filament\Resources\MediaConvertResource\Pages;
 
 use Filament\Resources\Pages\CreateRecord;
 use Modules\Media\Filament\Resources\MediaConvertResource;
+<<<<<<< HEAD
 use Modules\Xot\Filament\Resources\Pages\XotBaseCreateRecord;
 
 class CreateMediaConvert extends XotBaseCreateRecord
+=======
+
+class CreateMediaConvert extends \Modules\Xot\Filament\Resources\Pages\XotBaseCreateRecord
+>>>>>>> b5b5b268 (up)
 {
     protected static string $resource = MediaConvertResource::class;
 }
--- app_old/Filament/Resources/TemporaryUploadResource/Pages/CreateTemporaryUpload.php
+++ app_old/Filament/Resources/TemporaryUploadResource/Pages/CreateTemporaryUpload.php
@@ -6,9 +6,14 @@ namespace Modules\Media\Filament\Resources\TemporaryUploadResource\Pages;
 
 use Filament\Resources\Pages\CreateRecord;
 use Modules\Media\Filament\Resources\TemporaryUploadResource;
+<<<<<<< HEAD
 use Modules\Xot\Filament\Resources\Pages\XotBaseCreateRecord;
 
 class CreateTemporaryUpload extends XotBaseCreateRecord
+=======
+
+class CreateTemporaryUpload extends \Modules\Xot\Filament\Resources\Pages\XotBaseCreateRecord
+>>>>>>> b5b5b268 (up)
 {
     protected static string $resource = TemporaryUploadResource::class;
 }
