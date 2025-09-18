b5f7c7ea8097688ff024d1e7dd585748d676b184
marco76tv <marco.sottana@gmail.com>
2025-09-18 16:14:45 +0200

    .

--- docs/archived/config.php
+++ docs/archived/config.php
@@ -10,29 +10,25 @@ return [
     'siteName' => 'Modulo Media',
     'siteDescription' => 'Modulo Media',
     'lang' => 'it',
-
     'collections' => [
         'posts' => [
-            'path' => fn ($page): string => $page->lang.'/posts/'.Str::slug($page->getFilename()),
+            'path' => fn($page): string => $page->lang . '/posts/' . Str::slug($page->getFilename()),
         ],
         'docs' => [
-            'path' => fn ($page): string => $page->lang.'/docs/'.Str::slug($page->getFilename()),
+            'path' => fn($page): string => $page->lang . '/docs/' . Str::slug($page->getFilename()),
         ],
     ],
-
     // Algolia DocSearch credentials
     'docsearchApiKey' => env('DOCSEARCH_KEY'),
     'docsearchIndexName' => env('DOCSEARCH_INDEX'),
-
     // navigation menu
-    'navigation' => require_once (__DIR__.'/navigation.php'),
-
+    'navigation' => require_once __DIR__ . '/navigation.php',
     // helpers
-    'isActive' => fn ($page, $path) => Str::endsWith(trimPath($page->getPath()), trimPath($path)),
-    'isItemActive' => fn ($page, $item) => Str::endsWith(trimPath($page->getPath()), trimPath($item->getPath())),
+    'isActive' => fn($page, $path) => Str::endsWith(trimPath($page->getPath()), trimPath($path)),
+    'isItemActive' => fn($page, $item) => Str::endsWith(trimPath($page->getPath()), trimPath($item->getPath())),
     'isActiveParent' => function ($page, $menuItem) {
         if (is_object($menuItem) && $menuItem->children) {
-            return $menuItem->children->contains(fn ($child): bool => trimPath($page->getPath()) === trimPath($child));
+            return $menuItem->children->contains(fn($child): bool => trimPath($page->getPath()) === trimPath($child));
         }
     },
     'url' => function ($page, $path) {
@@ -41,10 +37,10 @@ return [
         }
 
         // return Str::startsWith($path, 'http') ? $path : '/' . trimPath($path);
-        return url('/'.$page->lang.'/'.trimPath($path));
+        return url('/' . $page->lang . '/' . trimPath($path));
     },
-
-    'children' => fn ($page, $docs): array =>
+    'children' => fn($_page, $_docs): array => (
         // return $docs->where('parent_id', $page->);
-        [],
+        []
+    ),
 ];
